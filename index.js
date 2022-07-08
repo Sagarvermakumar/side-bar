b2773e6b49c1d0c43f861867bc847c347b78c59d
sagarvermakumar <sksagarkumarverma@gmail.com>
2022-07-07 21:55:25 -1000

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let nevigation = document.querySelector('.nevigation');
+let toggle = document.querySelector('.toggle');
+
+toggle.addEventListener('click',()=>{
+    nevigation.classList.toggle('active');
+})
